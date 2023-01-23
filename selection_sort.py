bc341f420b138a0b9a898c7b18a910ba2a652969
Carljhn <zoletacarljohn1@gmail.com>
2023-01-22 16:08:33 -1200

    Third Commit: Implemented a nest for loop and an if statement.

--- selection_sort.py
+++ selection_sort.py
@@ -7,3 +7,6 @@ myNumbers = [24, 6, 7, 88, 51, 40, 11, 68, 79, 5]
 #for loop
 for i in range(len(myNumbers)):
     min_index = i #let i be the smallest number in the array
+    for j in range(i + 1, len(myNumbers)):
+        if myNumbers[j] < myNumbers[min_index]:
+            min_index = j 
