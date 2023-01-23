3b80f8f44a1790458a156475dccd88c77e9eae7e
Carljhn <zoletacarljohn1@gmail.com>
2023-01-22 18:59:58 -1200

    Third Commit: Implemented a while loop that is nested in the for loop.

--- insertion_sort.py
+++ insertion_sort.py
@@ -7,3 +7,10 @@ myNumbers = [24, 6, 7, 88, 51, 40, 11, 68, 79, 5]
 #for loop
 for i in range(1, len(myNumbers)):
     j = i 
+
+    #while loop
+    while myNumbers[j - 1] > myNumbers[j] and j > 0:
+        temp = myNumbers[j - 1]
+        myNumbers[j - 1] = myNumbers[j]
+        myNumbers[j] = temp
+        j -= 1
