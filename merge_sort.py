1cb80fdd0f0bc671c9fa205a53cd28d0307bd1f8
Carljhn <zoletacarljohn1@gmail.com>
2023-01-22 20:09:20 -1200

    Third Commit: Assigned value for variables i, j, and k.

--- merge_sort.py
+++ merge_sort.py
@@ -11,3 +11,8 @@ def merge_sort(myArray):
         #recursive iteration
         merge_sort(left_side)
         merge_sort(right_side)
+
+        #merge
+        i = 0 # this is for the left side array index
+        j = 0 # this is for the right side array index
+        k = 0 # this is for the merged array index
