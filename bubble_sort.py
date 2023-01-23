adda54f228501a339cd71d3e143358ea09010684
Carljhn <zoletacarljohn1@gmail.com>
2023-01-22 16:39:33 -1200

    Second Commit: Implemented a for loop.

--- bubble_sort.py
+++ bubble_sort.py
@@ -3,3 +3,13 @@
 
 #Assigned Numbers
 myNumbers = [24, 6, 7, 88, 51, 40, 11, 68, 79, 5]
+
+#for loop
+for i in range(len(myNumbers)-1, 0, -1):
+    for j in range(i):
+        if myNumbers[j] > myNumbers[j + 1]:
+
+            #swaps the position of the elements
+            temp = myNumbers[j]
+            myNumbers[j] = myNumbers[j + 1]
+            myNumbers[j + 1] = temp
